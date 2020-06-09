29a810bf37bef033d1c1a973ae0a7c640b84bf18
tonnkatubooy <tonnkatubooy@yahoo.co.jp>
2020-06-09 18:30:56 +0900

    edit

---
+++ src/js/client.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Layout extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome!</h1>
+        <h1>It works!</h1>
+
+      </div>
+    );
+  }
+}
+// const text = "テキスト";
+
+const app = document.getElementById('app');
+ReactDOM.render(<Layout/>, app);
