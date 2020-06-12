ebc71f709bce633358bd57e7d49643fb69aa4936
tonnkatubooy <tonnkatubooy@yahoo.co.jp>
2020-06-12 13:15:25 +0900

    change title

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -1,18 +1,24 @@
 import React from "react";
-import Header from "./Header";
-import Footer from "./Footer";
+import Header from "./Header";  //ヘッダーをインポート
+import Footer from "./Footer";  //フッターをインポート
 
 export default class Layout extends React.Component {
-  // constructor() {
-  //   super();
-  //   this.state = {name: "Tsutomu"}
-  //  }
+  constructor() {
+    super();
+    this.state = {title: "Welcome"}
+   }
+   changeTitle(title){
+     this.setState({title});
+   }
   render() {
-    const title = "welcome Tsutomu!"
+    // // const title = "welcome Tsutomu!"
+    // setTimeout(
+    //   () => {this.setState({title: "Welcome Tsutomu"});},  //2秒後にsetStateメソッドで置き換わる
+    //   2000
+    // )
     return (
       <div>
-        <Header title={title} />
-        <Header title={"thank you!"} />
+        <Header changeTitle={this.changeTitle.bind(this)} title={this.state.title} />
         <Footer />
       </div>    //変数も定義できる。例) const text = "テキストです";  <div>{text}</div> 
     );
