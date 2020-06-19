736d16e1d60879c2b28d3d340dc30a52c586fe4b
tonnkatubooy <tonnkatubooy@yahoo.co.jp>
2020-06-19 13:30:55 +0900

    Update title.js

--- src/js/components/Header/title.js
+++ src/js/components/Header/title.js
@@ -3,7 +3,14 @@ import React from "react";
 export default class Title extends React.Component{    //classはファイル名と同じ名前？？ この構文は丸暗記
   render(){
     return(
-      <h1>{this.props.title}</h1>
+      <div>
+        <h1>{this.props.title}</h1>
+        <h2>{this.get_result(3)}</h2>
+      </div>
+      
     );
+  }
+  get_result(num){
+    return 1 + num;
   }
 } 
